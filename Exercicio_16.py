f5a229ff1138923f0d9d5b297e8554cd521e3bd5
robeertgr <robeertgr@live.com>
2021-10-18 15:24:28 -0300

    Resolução exercícios 16 e 17

---
+++ Exercicio_16.py
@@ -0,0 +1,5 @@
+polegadas = float(input("Digite o valor do comprimento em polegadas: "))
+
+centimetros = polegadas * 2.54
+
+print(f"O valor {polegadas} pol convertido em centimetros é {round(centimetros, 2)} cm")
---
+++ Exercicio_17.py
@@ -0,0 +1,6 @@
+centimetros = float(input("Digite um valor de comprimento em centimetros: "))
+
+polegadas = centimetros / 2.54
+
+print(f"O valor {centimetros} cm convertido em polegadas é {round(polegadas, 2)} pol")
+
