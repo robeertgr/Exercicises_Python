f6d944da3149700d83239cbcff0ba1fa8e73cfb1
robeertgr <robeertgr@live.com>
2021-10-18 22:47:29 -0300

    Resolução dos exercicios 18 a 27 do curso de Python da Geek University

---
+++ Exercicio_22.py
@@ -0,0 +1,5 @@
+jardas = float(input("Digite um comprimento em jardas: "))
+
+metros = 0.91 * jardas
+
+print(f"O valor {jardas} jardas convertido em metros é {round(metros, 2)} metros")
---
+++ Exercicio_24.py
@@ -0,0 +1,5 @@
+metros = float(input("Digite um valor de área em metros quadrado: "))
+
+acres = metros * 0.000247
+
+print(f"O valor {metros} metros convertido em acres é {round(acres, 2)} acres")
