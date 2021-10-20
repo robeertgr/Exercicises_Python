f9d4e0dee2d73dc0d3730dc6bf2cd5c6b7c7859b
robeertgr <robeertgr@live.com>
2021-10-19 21:41:15 -0300

    Correção do exercício

--- Exercicio_30.py
+++ Exercicio_30.py
@@ -1,6 +1,6 @@
 real = float(input("Digite o valor em reais: "))
 cotacao = float(input("Digite a cotacao do dolar: "))
 
-conversao = real / cotacao
+conversao = real * cotacao
 
 print(f"{round(real, 2)} reais convertido em dólar são {round(conversao, 2)} dólares")
